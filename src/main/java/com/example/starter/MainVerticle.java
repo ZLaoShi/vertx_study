a48d891370e533478cfe7a77a87a9bcbb3e91f12
mxwj <z2116344865@gmail.com>
2025-03-16 17:12:36 +0000

    使用Future/Promise增加可读性

--- src/main/java/com/example/starter/MainVerticle.java
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -9,6 +9,7 @@ import graphql.schema.idl.SchemaGenerator;
 import graphql.schema.idl.SchemaParser;
 import graphql.schema.idl.TypeDefinitionRegistry;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
@@ -19,70 +20,75 @@ import io.vertx.ext.web.handler.graphql.GraphiQLHandlerOptions;
 import io.vertx.ext.web.handler.graphql.instrumentation.JsonObjectAdapter;
 
 public class MainVerticle extends AbstractVerticle {
-  private UserService userService;
+    private UserService userService;
 
     @Override
-    public void start(Promise<Void> startPromise) throws Exception {
-        // 1. 异步读取 schema 文件
-        vertx.fileSystem().readFile("schema.graphqls")
-            .compose(buffer -> {
-                // 2. 异步初始化 UserService
-                return vertx.executeBlocking(() -> {
-                    userService = new UserService();
-                    
-                    // 3. 设置 GraphQL
-                    String schema = buffer.toString();
-                    SchemaParser schemaParser = new SchemaParser();
-                    TypeDefinitionRegistry typeDefinitionRegistry = schemaParser.parse(schema);
-                    
-                    RuntimeWiring runtimeWiring = RuntimeWiring.newRuntimeWiring()
-                        .type("Query", builder ->
-                            builder.dataFetcher("user", environment -> {
-                                String idStr = environment.getArgument("id");
-                                Integer id = Integer.parseInt(idStr);
-                                return userService.findById(id)
-                                    .subscribeAsCompletionStage();
-                            })
-                        )
-                        .build();
-
-                    SchemaGenerator schemaGenerator = new SchemaGenerator();
-                    GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(
-                        typeDefinitionRegistry, 
-                        runtimeWiring
-                    );
-
-                    return GraphQL.newGraphQL(graphQLSchema)
-                        .instrumentation(new JsonObjectAdapter())
-                        .build();
-                });
-            })
-            .onSuccess(graphQL -> {
-                // 4. 设置路由
-                Router router = Router.router(vertx);
-                router.route().handler(BodyHandler.create());
-                
-                router.route("/graphql").handler(
-                    GraphQLHandler.create(graphQL, new GraphQLHandlerOptions()
-                        .setRequestBatchingEnabled(true))
-                );
-                
-                router.route("/graphiql/*").handler(
-                    GraphiQLHandler.create(vertx, new GraphiQLHandlerOptions()
-                        .setEnabled(true)
-                        .setGraphQLUri("/graphql"))
-                );
-                
-                // 5. 启动服务器
-                vertx.createHttpServer()
-                    .requestHandler(router)
-                    .listen(8888)
-                    .onSuccess(server -> {
-                        System.out.println("HTTP server started on port " + server.actualPort());
-                        startPromise.complete();
-                    })
-                    .onFailure(startPromise::fail);
-            })
+    public void start(Promise<Void> startPromise) {
+        initializeUserService()
+            .compose(this::setupGraphQL)
+            .compose(this::setupRouter)
+            .onSuccess(v -> startPromise.complete())
             .onFailure(startPromise::fail);
     }
+
+    private Future<Void> initializeUserService() {
+        return vertx.executeBlocking(() -> {
+            userService = new UserService();
+            return null;
+        });
+    }
+
+    private Future<GraphQL> setupGraphQL(Void v) {
+        return vertx.fileSystem()
+            .readFile("schema.graphqls")
+            .map(buffer -> {
+                String schema = buffer.toString();
+                SchemaParser schemaParser = new SchemaParser();
+                TypeDefinitionRegistry typeRegistry = schemaParser.parse(schema);
+                
+                RuntimeWiring runtimeWiring = RuntimeWiring.newRuntimeWiring()
+                    .type("Query", builder ->
+                        builder.dataFetcher("user", env -> {
+                            Integer id = Integer.parseInt(env.getArgument("id"));
+                            return userService.findById(id)
+                                .subscribeAsCompletionStage();
+                        })
+                    )
+                    .build();
+
+                GraphQLSchema graphQLSchema = new SchemaGenerator()
+                    .makeExecutableSchema(typeRegistry, runtimeWiring);
+
+                return GraphQL.newGraphQL(graphQLSchema)
+                    .instrumentation(new JsonObjectAdapter())
+                    .build();
+            });
+    }
+
+    private Future<Void> setupRouter(GraphQL graphQL) {
+        Router router = Router.router(vertx);
+        router.route().handler(BodyHandler.create());
+        
+        router.route("/graphql").handler(
+            GraphQLHandler.create(graphQL, 
+                new GraphQLHandlerOptions()
+                    .setRequestBatchingEnabled(true))
+        );
+        
+        router.route("/graphiql/*").handler(
+            GraphiQLHandler.create(vertx, 
+                new GraphiQLHandlerOptions()
+                    .setEnabled(true)
+                    .setGraphQLUri("/graphql"))
+        );
+        
+        return vertx.createHttpServer()
+            .requestHandler(router)
+            .listen(8888)
+            .map(server -> {
+                System.out.println("Server started on port " + server.actualPort());
+                return null;
+            });
+    }
 }
+ 
