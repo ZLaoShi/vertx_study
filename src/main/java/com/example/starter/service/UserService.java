507259abc3eaaa56b1f7321b423020c7553c64cd
mxwj <z2116344865@gmail.com>
2025-03-16 16:25:55 +0000

    更优雅的orm初始化

--- src/main/java/com/example/starter/service/UserService.java
+++ src/main/java/com/example/starter/service/UserService.java
@@ -1,18 +1,21 @@
 package com.example.starter.service;
 
 import com.example.starter.model.User;
-import org.hibernate.reactive.mutiny.Mutiny;
+// import org.hibernate.reactive.mutiny.Mutiny;
+// import org.hibernate.reactive.mutiny.Mutiny.SessionFactory;
+
 import io.smallrye.mutiny.Uni;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
+
+import static jakarta.persistence.Persistence.createEntityManagerFactory;
+import static org.hibernate.reactive.mutiny.Mutiny.SessionFactory;
 
 public class UserService {
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("pg-vertx-study");
+    SessionFactory factory =
+				createEntityManagerFactory( "pg-vertx-study")
+						.unwrap(SessionFactory.class);
 
     public Uni<User> findById(Integer id) {
-        Mutiny.SessionFactory sessionFactory = emf.unwrap(Mutiny.SessionFactory.class);
-
-        return sessionFactory.withSession(session -> 
+        return factory.withSession(session -> 
         session.find(User.class, id)
         ).onFailure().invoke(error -> {
             System.err.println("数据库查询错误: " + error.getMessage());
